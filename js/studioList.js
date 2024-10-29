2f66b18450ade3c0dc15f69f199a34549d6cbd9c
Vit <h12113999@s.wu.ac.at>
2024-10-29 20:01:38 +0100

    List of studios added

---
+++ js/studioList.js
@@ -0,0 +1,34 @@
+const musicStudios = [
+    {
+        name: "Studio A",
+        price: 50,
+        district: "1010",
+        size: "500 sq ft"
+    },
+    {
+        name: "Studio B",
+        price: 75,
+        district: "1210",
+        size: "750 sq ft"
+    },
+    {
+        name: "Studio C",
+        price: 100,
+        district: "1100",
+        size: "1000 sq ft"
+    },
+    {
+        name: "Studio D",
+        price: 60,
+        district: "1180",
+        size: "600 sq ft"
+    },
+    {
+        name: "Studio E",
+        price: 80,
+        district: "1040",
+        size: "800 sq ft"
+    }
+];
+
+console.log(musicStudios);
