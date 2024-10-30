d3a3d9cd0fc1588f99bfbb9ad43ee3a9c8c6d33a
Vit <h12113999@s.wu.ac.at>
2024-10-30 22:09:02 +0100

    Adding a few studios with JS

--- js/studioList.js
+++ js/studioList.js
@@ -1,38 +1,41 @@
 const musicStudios = [
     {
         name: "Studio A",
+        type: "Recording",
         price: 50,
         district: "1010",
         size: "500 sq ft"
     },
     {
         name: "Studio B",
+        type: "Rehearsal",
         price: 75,
         district: "1210",
         size: "750 sq ft"
     },
     {
         name: "Studio C",
+        type: "Production",
         price: 100,
         district: "1100",
         size: "1000 sq ft"
     },
     {
         name: "Studio D",
+        type: "Recording",
         price: 60,
         district: "1180",
         size: "600 sq ft"
     },
     {
         name: "Studio E",
+        type: "Rehearsal",
         price: 80,
         district: "1040",
         size: "800 sq ft"
     }
 ];
-// for (let i = 0; i < musicStudios.length; i++) {
-//     console.log(musicStudios[i].name);
-// }
+
 var HTMLlist = document.getElementById("cards");
 if (!HTMLlist) {
     console.error("Element with ID 'cards' not found in the DOM.");
@@ -49,10 +52,11 @@ newElement.setAttribute("id", "cardsToCreate");
 
 for (let i = 0; i < musicStudios.length; i++) {
     let newElement = document.createElement("div"); 
-    newElement.setAttribute("class", "card sm-3 m-4 col-3"); 
-    newElement.setAttribute("style", "width: 18rem; height: 18rem"); 
+    newElement.setAttribute("class", "card lg-3 m-4 col-3"); 
+    newElement.setAttribute("style",  "max-width: 22rem; min-width: 16rem; max-height: 22rem; min-height: 16rem;"); 
     newElement.setAttribute("id", "cardsToCreate"); 
         const studioName = musicStudios[i].name;
+        const studioType = musicStudios[i].type;
         const studioPrice = musicStudios[i].price;
         const studioDistrict = musicStudios[i].district;
         const studioSize = musicStudios[i].size;
@@ -60,7 +64,7 @@ for (let i = 0; i < musicStudios.length; i++) {
             <div class="card-body">
                 <div class="row">
                     <div class="col-12"><h3>${studioName}</h3></div>
-                    <h5 class="card-title">${studioName}</h5>
+                    <h5 class="card-title">${studioType}</h5>
                     <p class="card-text">Price: ${studioPrice}</p>
                     <p class="card-text">District: ${studioDistrict}</p>
                     <p class="card-text">Size: ${studioSize}</p>
@@ -71,22 +75,5 @@ for (let i = 0; i < musicStudios.length; i++) {
         HTMLlist.appendChild(newElement);
         
     }
-    // for (let i = 0; i < musicStudios.length; i++) {
-    //     console.log(HTMLlist[i]);
-    // }
+   
 
-// to add it to the body: 
-// or to add it to some element: 
-
- //  <div class="card sm-3 m-4 col-3" style="width: 18rem; height: 18rem;">
-        //     <div class="card">
-        //         <div class="row">
-        //             <div class="card-body">
-        //                 <h5 class="card-title">${studioName}</h5>
-        //                 <p class="card-text">Price: ${studioPrice}</p>
-        //                 <p class="card-text">District: ${studioDistrict}</p>
-        //                 <p class="card-text">Size: ${studioSize}</p>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
