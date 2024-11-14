6179062cc4883042a499a135d5204f08b922d27a
daiv-id <186632169+daiv-id@users.noreply.github.com>
2024-11-14 20:45:51 +0100

    corrected seeMore.js

--- js/seeMore.js
+++ js/seeMore.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-    $('#seeMoreStudios').click(function(){
-      // Slide down additional studio rows
-      $('.more-studios').slideDown("slow");
-      
-      // Hide the 'See more studios' button
-      $(this).hide();
-      
-      // Show the 'See all studios' link
-      $('#seeAllStudios').show();
+  $('#seeMoreStudios').click(function(){
+    // Slide down additional studio rows
+    $('.more-studios').slideDown("slow");
+
+    // Hide the 'See more studios' button
+    $(this).hide();
+
+    // Show the 'See all studios' link
+    $('#seeAllStudios').show();
+  });
 });
