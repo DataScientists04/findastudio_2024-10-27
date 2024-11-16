3ef4e35554c4b79363abc8637945a61cceba5ada
daiv-id <186632169+daiv-id@users.noreply.github.com>
2024-11-16 21:02:40 +0100

    My Reservation V2

--- pages/MyReservations.php
+++ pages/MyReservations.php
@@ -20,14 +20,18 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/FindAStudio/php/ConnectDB.php";
     <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/FindAStudio/pages/navbar.php"; ?>
 
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-2">
       </div>
-      <div class="col-md-6">
+      <div class="col-md-8 text-center">
         <h2 class="pb-3">Your Reservations</h2>
+        <p class="pb-3">Welcome to your reservations page! Here, you'll find all the details about your upcoming studio bookings.</p>
+        <p class="pb-3">If you're looking to book more studio time, check out our available studios <a href="/FindAStudio/pages/studioList.php"><u>here</u></a>.
+        We have a wide range of options to suit your needs, whether you're a professional artist or a weekend rockstar.
+        </p>
         <?php
         if (isset($_SESSION['UserID'])) {
           $UserID = $_SESSION['UserID'];
-          $query = "SELECT r.Reservation_Date, s.Studio_Name
+          $query = "SELECT r.Reservation_Date, s.Studio_name, s.City, s.Postal_code, s.Street, s.street_no, s.Type, s.Price
                     FROM reservation r
                     JOIN studio s ON r.StudioID = s.StudioID
                     WHERE r.UserID = ?
@@ -39,10 +43,73 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/FindAStudio/php/ConnectDB.php";
 
           if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-              echo "<p class='pb-3'>Reservation Date: " . $row['Reservation_Date'] . "<br>Studio: " . $row['Studio_Name'] . "</p>";
+              $Studio_name = $row['Studio_name'];
+              $City = $row['City'];
+              $Postal_code = $row['Postal_code'];
+              $Street = $row['Street'];
+              $street_no = $row['street_no'];
+              $Type = $row['Type'];
+              $Price = $row['Price'];
+              $Reservation_Date = $row['Reservation_Date'];
+              ?>
+              <div class="row d-flex align-items-end border-top">
+                <div class="row pt-3 py-1">
+                  <div class="col-6">
+                    <?php echo "<h3>" . "Reservation Date: " . "</h3>"; ?>
+                  </div>
+                  <div class="col-6">
+                    <?php echo "<h3>" . $Reservation_Date . "</h3>"; ?>
+                  </div>
+                </div>
+
+                <div class="row py-1">
+                  <div class="col-6">
+                    <?php echo "<h3>" . "Studio Name: " . "</h3>"; ?>
+                  </div>
+                  <div class="col-6">
+                    <?php echo "<h3>" . $Studio_name . "</h3>"; ?>
+                  </div>
+                </div>
+
+                <div class="row py-1">
+                  <div class="col-6">
+                    <?php echo "<h4>" . "Address: " . "</h4>"; ?>
+                  </div>
+                  <div class="col-6">
+                    <?php echo "<h4>" . $City . ", " . $Postal_code . ", " . $Street . " " . $street_no . "</h4>"; ?>
+                  </div>
+                </div>
+
+                <div class="row py-1">
+                  <div class="col-6">
+                    <?php echo "<h4>" . "Type of Studio: " . "</h4>"; ?>
+                  </div>
+                  <div class="col-6">
+                    <?php echo "<h4>" . $Type . "</h4>"; ?>
+                  </div>
+                </div>
+
+                <div class="row py-1">
+                  <div class="col-6">
+                    <?php echo "<h4>" . "Price: " . "</h4>"; ?>
+                  </div>
+                  <div class="col-6">
+                    <?php echo "<h4>" . $Price . "€" . "</h4>"; ?>
+                  </div>
+                </div>
+              </div>
+              <?php
             }
           } else {
-            echo "<p class='pb-3'>You have no reservations.</p>";
+            ?>
+            <div class="row d-flex align-items-end border-top">
+              <div class="row pt-3 py-1" style="height: 25vh;">
+                <div class="col-12">
+                  <h2>You currently have no reservations</h2>
+                </div>
+              </div>
+            </div>
+            <?php
           }
 
           $stmt->close();
@@ -54,7 +121,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/FindAStudio/php/ConnectDB.php";
       }
         ?>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-2">
       </div>
     </div>
 
