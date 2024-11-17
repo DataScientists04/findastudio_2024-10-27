ffb9d830bfea472fc42ecf0c40712c9d6c9b8df0
daiv-id <186632169+daiv-id@users.noreply.github.com>
2024-11-17 12:44:47 +0100

    iframe map fix 2

--- index.php
+++ index.php
@@ -151,7 +151,8 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/FindAStudio/php/ConnectDB.php";
       <div class="col-md-3">
       </div>
       <div class="col-md-6">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d42540.035937471235!2d16.35414655519814!3d48.21138787178054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smusic%20studio!5e0!3m2!1sen!2sat!4v1730152479933!5m2!1sen!2sat" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d42540.035937471235!2d16.35414655519814!3d48.21138787178054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smusic%20studio!5e0!3m2!1sen!2sat!4v1730152479933!5m2!1sen!2sat"
+        width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
       <div class="col-md-3">
       </div>
